54aebe63d574dc7cbb8e94568422cf7407c1141d
David Tamayo <dtmayo@birt.eus>
2023-12-01 12:33:48 +0100

    ejercicio7

--- src/app/tabs/tab4/tab4.page.ts
+++ src/app/tabs/tab4/tab4.page.ts
@@ -10,20 +10,25 @@ import { IonLabel } from '@ionic/angular';
 export class Tab4Page implements OnInit {
  //numero aleatorio creado al principio al lanzar la aplicacion
   numAle: number= 0;
-  numUsu: number = 0;
+  numUsu :number = 0;
+  numIntentos: number = 0;
   mensaje: string = "";
+  labelNumero: string = "Itroduzca un número"
+  numero?: number;
   
 
   constructor() { }
 
   onClick(){
     console.log(this.numAle);
-    console.log(this.numAle);
-   this.comprobarNumAle();
+    console.log(this.numero);
+   this.comprobarNumAle(); 
+  
   }
   //función que se ejecuta unicamente una vez al inicio de la aplicacion
   ngOnInit() {
     this.generarNumAle();
+    console.log("se ha generado un nuevo numero aleatorio");
   }
   generarNumAle(){
   // Math.floor(Math.random() * 100) + 1;
@@ -31,19 +36,60 @@ export class Tab4Page implements OnInit {
    return  this.numAle;
     
   }
+  public alertButtons = [
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      handler: () => {
+        console.log('Alert canceled');
+      },
+    },
+    {
+      text: 'OK',
+      role: 'confirm',
+      handler: () => {
+        console.log('Alert confirmed');
+        this.reiniciarJuego();
+        this.generarNumAle();
+        console.log("se ha generado un nuevo numero aleatorio");
+      },
+    },
+  ];
+
+  reiniciarJuego(){
+    this.numero= undefined;
+    this.numAle = 0;
+    this.numUsu = 0;
+    this.numIntentos = 0;
+    this.mensaje = "";
+    console.log(this.numero)
+  }
   comprobarNumAle(){
-    
-    //el segundo caso seria cuando el numero introducido es un numero negativo o mayor que 100
-    if (this.numAle == this.numUsu) {
-      this.mensaje = "Has acertado";   
-    }  else if (this.numUsu < 0 || this.numUsu > 100) {
-      this.mensaje = "Introduce un número entre 0 y 100";
-    } else if (this.numAle < this.numUsu) {
-      this.mensaje = "Introduce un número menor";
-    } else if (this.numAle > this.numUsu) {  
-      this.mensaje = "Introduce un número mayor";
+    //en el caso de que en la vista aya algo escrito en el campo imput
+    //asignara el valor y procedera a comparar
+    if (this.numero != null){
+      this.numUsu= this.numero;
+      if (this.numAle == this.numUsu) {
+        this.mensaje = "enhorabuena!! Has acertado el número secreto."; 
+      
+        //en el caso de que el valor introducido este fuera de los numeros 0-100
+      } else if (this.numUsu < 0 || this.numUsu > 100) {
+        this.mensaje = "Introduce un número entre 0 y 100";
+        
+      } else if (this.numAle < this.numUsu) {
+        this.mensaje = "Introduce un número menor";
+        this.numIntentos++;
+       
+      } else if (this.numAle > this.numUsu) {  
+        this.mensaje = "Introduce un número mayor";
+        this.numIntentos++;
+
+      }
+      //en el caso de que no aya nada escrito saldran un mensaje diciendo que no se ha introducido ningun número
+    }else if (this.numero == undefined){
+      this.mensaje = "El campo esta vacio, Introduce un numero";
+      console.log("El campo esta vacio, Introduce un numero");
     }
-    
   }
 
 }
